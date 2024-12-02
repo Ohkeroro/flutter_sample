a6d4a5cf5674102619cbaa1c69fcb5082a830d78
Ohkeroro <3651051541144@rmutr.ac.th>
2024-12-02 12:23:08 +0700

    third commit

---
+++ flutter_sample/lib/my_scroll.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MyScrollView extends StatelessWidget {
+  const MyScrollView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ScrollView'),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Row(
+            children: [
+              boxContainer('one'),
+              boxContainer('two'),
+              boxContainer('three'),
+              boxContainer('four'),
+              boxContainer('five'),
+              boxContainer('six'),
+              boxContainer('seven'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget boxContainer(String txt) => Container(
+    width: 100,
+    height: 150,
+    color: Colors.cyan.shade300 ,
+    margin: const EdgeInsets.all(20),
+    alignment: Alignment.center,
+    child: Text(txt, style: const TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+  );
+    
+}
